bf100d32beea0df02e8613533053709327a2fe83
zdying <zdying@live.com>
2016-09-04 20:23:22 +0800

    👏 add_cookie and hide_cookie support

--- src/proxy/commands/index.js
+++ src/proxy/commands/index.js
@@ -14,15 +14,15 @@ module.exports = {
         delete this.request.headers[key];
     },
     'proxy_set_cookie': function(key, value){
-
+        log.debug('proxy_set_cookie -', this, key, value);
     },
-    'proxy_del_cookie': function(key, value){
-
+    'proxy_del_cookie': function(key){
+        log.debug('proxy_del_cookie -', this, key, value);
     },
 
     // ## response config
-    'hide_cookie': function(key, value){
-
+    'hide_cookie': function(key){
+        this.response.headers['Set-Cookie'] = key + '=; Expires=' + new Date(1);
     },
     'hide_header': function(key, value){
         log.debug('hide_header -', this, key, value);
@@ -33,6 +33,7 @@ module.exports = {
         this.response.headers[key] = value;
     },
     'set_cookie': function(key, value){
-
+        log.debug('set_cookie -', this, key, value);
+        this.response.headers['Set-Cookie'] = key + '=' + value;
     }
 };
