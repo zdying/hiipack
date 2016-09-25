c6f05d28a828d55d61db9c5cc60dfdd68cf4eca1
zdying <zdying@live.com>
2016-09-25 14:47:58 +0800

    change

--- opt.js
+++ opt.js
@@ -7,21 +7,25 @@
 let fs = require('fs');
 let path = require('path');
 
-fs.readdir('node_modules', (err, files) => {
-    var total = files.length;
+fs.readdir('node_modules', (err, modules) => {
+    var total = modules.length;
 
     console.log('node_modules共有', total, '个模块');
 
     var result = [];
 
-    files.forEach((file, index) => {
-        let stat = fs.statSync('node_modules/' + file);
+    modules.forEach((module, index) => {
+        if(module === '.bin'){
+            return
+        }
+
+        let stat = fs.statSync('node_modules/' + module);
         if(stat.isDirectory()){
             // 查找压缩的代码
-            // console.log('==>', 'finding', 'node_modules/' + file);
-            let minFile = path.resolve('node_modules', file, 'dist', file + '.min.js');
-            let minFile1 = path.resolve('node_modules', file, 'dist', file + '.js');
-            let minFile2 = path.resolve('node_modules', file, file + '.min.js');
+            // console.log('==>', 'finding', 'node_modules/' + module);
+            let minFile = path.resolve('node_modules', module, 'dist', module + '.min.js');
+            let minFile1 = path.resolve('node_modules', module, 'dist', module + '.js');
+            let minFile2 = path.resolve('node_modules', module, module + '.min.js');
 
             // console.log(minFile);
             // console.log(minFile1);
@@ -29,26 +33,30 @@ fs.readdir('node_modules', (err, files) => {
             if(fs.existsSync(minFile)){
                 result.push(minFile);
                 check();
-                // return console.log('==> min file detected: ', minFile);
-            }
-
-            if(fs.existsSync(minFile1)){
+                // return console.log('==> min module detected: ', minFile);
+            }else if(fs.existsSync(minFile1)){
                 result.push(minFile1);
                 check();
-                // return console.log('==> min file detected: ', minFile1);
+                // return console.log('==> min module detected: ', minFile1);
             }
 
             if(fs.existsSync(minFile2)){
                 result.push(minFile2);
-                // return console.log('==> min file detected: ', minFile2);
+                // return console.log('==> min module detected: ', minFile2);
             }
 
             // 只有一个js文件的模块
-            fs.readdir('node_modules/' + file, function(err, files){
+            fs.readdir('node_modules/' + module, function(err, files){
                 files = files.filter(f => f.split('.').pop() === 'js');
 
                 if(files.length === 1){
-                    result.push('node_modules/' + file + '/' + files[0]);
+                    var info = require(module + '/package.json');
+                    var dep = info.dependencies;
+                    var deps = Object.keys(dep);
+
+                    if(deps === 0){
+                        result.push('node_modules/' + file + '/' + files[0]);
+                    }
                 }
 
                 check();
