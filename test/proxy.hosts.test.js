3d27f7d2b4f64328bfdd3a45c6195736cb0c9578
zdying <zdying@live.com>
2016-10-18 11:23:09 +0800

    add hosts test

---
+++ test/proxy.hosts.test.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @author zdying
+ */
+
+'use strict';
+
+var assert = require('assert');
+var path = require('path');
+
+var parseHosts = require('../src/proxy/parseHosts');
+
+describe('proxy hosts',function(){
+    it('正确解析hosts文件', function(){
+        var hostsObj = parseHosts(path.resolve(__dirname, 'proxy/hosts'));
+        var target = {
+            'hiipack.com': '127.0.0.1:8800',
+            'hii.com': '127.0.0.1:8800',
+            'example.com': '127.0.0.1',
+            'example.com.cn': '127.0.0.1'
+        };
+
+        assert(JSON.stringify(hostsObj) === JSON.stringify(target))
+    });
+});
