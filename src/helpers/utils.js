c5293c28b385308420554ae8ae11e1a71d3eecfb
zdying <zdying@live.com>
2016-11-26 18:04:49 +0800

    支持非启动目录"根目录"的项目

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -7,7 +7,13 @@ var path = require('path');
 
 module.exports = {
     getProjectTMPDIR: function(root){
-        var projectName = root.replace(/\/$/, '').split(/[\/\\{1,2}]/).pop();
+        //TODO 可以把前面获取到projInfo传递到这里
+        // var projectName = root.replace(/\/$/, '').split(/[\/\\{1,2}]/).pop();
+        var projectName = root
+            .replace(/\/$/, '')
+            .replace(__hii__.cwd, '')
+            .replace(/^[\/\\]/, '');
+
         var tmpDir = path.resolve(__hii__.codeTmpdir, projectName);
 
         log.debug('webpackConfig -', projectName.bold.green, 'tmp dir', tmpDir);
