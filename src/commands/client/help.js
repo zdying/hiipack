679bcb39fb6d87f98fb81825059b5d7c2d4203f7
zdying <zdying@live.com>
2016-12-06 09:38:23 +0800

    2.0.0

---
+++ src/commands/client/help.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @author zdying
+ */
+
+module.exports = {
+    exec: function(args){
+        console.log('help info will coming soon...');
+    },
+
+    usage: function(){
+        return "hiipack clear";
+    },
+
+    help: function(){
+        return null;
+    }
+}
